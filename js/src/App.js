42025e228da77bd6ebe59d03075e30cf5bcf02f5
Sumeet Vaidya <suvaidya@gmail.com>
2017-08-17 17:25:59 -0700

    Update main page
    Summary: Add tabs from react-bootstrap and get the react-bootstrap CSS working. Adding modules for each of the tabs next
    
    Test Plan: npm start

--- js/src/App.js
+++ js/src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+
+import Tab from 'react-bootstrap/lib/Tab';
+import Tabs from 'react-bootstrap/lib/Tabs';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,11 +12,16 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Old Man Sumeet's D&D Thing</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Tabs id="app-tab-selector">
+          <Tab eventKey="Characters" title="Characters">
+            Characters
+          </Tab>
+          <Tab eventKey="Classes" title="Classes">
+            Classes
+          </Tab>
+        </Tabs>
       </div>
     );
   }
